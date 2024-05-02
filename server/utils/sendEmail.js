13135a8ad3ee11dd81716db8db5b98f3d938a1fe
afrazrajpoot <afrazrajpoot46@gmail.com>
2024-05-02 13:48:50 +0500

    add something

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,28 +1,27 @@
 const nodemailer = require("nodemailer");
 
 module.exports = async (email, subject, text) => {
-	try {
-		const transporter = nodemailer.createTransport({
-			host: process.env.HOST,
-			service: process.env.SERVICE,
-			port: Number(process.env.EMAIL_PORT),
-			secure: Boolean(process.env.SECURE),
-			auth: {
-				user: process.env.USER,
-				pass: process.env.PASS,
-			},
-		});
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL, // generated ethereal user
+        pass: process.env.PASSWORD, // generated ethereal password
+      },
+    });
 
-		await transporter.sendMail({
-			from: process.env.USER,
-			to: email,
-			subject: subject,
-			text: text,
-		});
-		console.log("email sent successfully");
-	} catch (error) {
-		console.log("email not sent!");
-		console.log(error);
-		return error;
-	}
+    await transporter.sendMail({
+      from: process.env.USER,
+      to: email,
+      subject: subject,
+      text: text,
+    });
+    console.log("email sent successfully");
+  } catch (error) {
+    console.log("email not sent!");
+    console.log(error);
+    return error;
+  }
 };
